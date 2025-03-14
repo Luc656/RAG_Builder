bf1c21073d972c0c25870b6675de6978d6b04e43
Luc656 <luc@daviesboys.co.uk>
2025-03-14 22:19:23 +0000

    testing news APIs

--- news_data_pipeline.py
+++ news_data_pipeline.py
@@ -4,30 +4,32 @@ import time
 import os
 import json
 
-API_KEY = os.getenv("NEWS_API_KEY")
-site = "https://newsapi.org/v2/everything"
+API_KEY = os.getenv("NEWS_DATA_API_KEY")
+print(API_KEY)
+site = "https://newsdata.io/api/1/latest"
 
 params = {
     'q' : 'geopolitics OR international affairs OR diplomacy OR war OR global economy',
     'apiKey' : API_KEY,
     'language' : 'en',
-    'sources' : "bbc-news, al-jazeera-english, the-guardian-uk, reuters"
+    #'full_content' : '1'
+    #'sources' : "bbc-news, al-jazeera-english, the-guardian-uk, reuters"
 }
 
 resp = requests.get(site,params=params)
 
 print(resp.status_code)
 
+print(resp.json())
+
 print(resp.json()['totalResults'])
-print(resp.json()['articles'])
+print(resp.json()['results'])
 
 print()
 
-articles = resp.json()['articles']
+with open('test_news_data.txt', 'w') as f1:
 
-with open('test.txt', 'w') as f1:
-
-    json.dump(resp.json()['articles'], f1, indent=4)
+    json.dump(resp.json()['results'], f1, indent=4)
 
 
 
