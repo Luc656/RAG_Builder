cbde7a25320bb357ff9be1799372a9fa2fa27a7e
Luc Davies <luc@daviesboys.co.uk>
2025-05-13 21:28:38 +0100

    building retreiver first steps

---
+++ rag_main.py
@@ -0,0 +1,12 @@
+import weaviate
+from sentence_transformers import SentenceTransformer
+
+class RAG:
+
+    def __init__(self):
+        pass
+
+    def retreive_chunks(self, query, k, metadata_filter):
+        pass
+
+        # embed user query
