9001607fadd0a8c1f54a3b0e272e5551211d9f9a
Luc Davies <luc@daviesboys.co.uk>
2025-05-17 15:42:16 +0100

    planning

--- out_pipeline.py
+++ out_pipeline.py
@@ -2,5 +2,12 @@ from processor import Processor
 from retriever import Retriever
 from blog_scraper_simple import WebScraper
 
+# steps:
+#   1. accept query
+#   2. retrieve data
+#   3. format response & build prompt
+#   4. send to LLM
+
 def out_pipeline():
     pass
+
